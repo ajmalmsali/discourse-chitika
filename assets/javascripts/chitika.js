6978666c28b7d2a77490ddda16dd12f2ddce6fee
Ajmal Sali <ajmalmsali@gmail.com>
2014-11-02 23:44:30 +0530

    Update chitika.js

--- assets/javascripts/chitika.js
+++ assets/javascripts/chitika.js
@@ -11,7 +11,7 @@
       if (eval('Discourse.SiteSettings.chitika_show_' + slotid)) {
         return new Handlebars.SafeString('<center><div id="'+slotid+'"></div></center>'+
         '<script type="text/javascript">'+
-          'window.onload = function() {'+
+          'function chitika_load() {'+
           'if (window.CHITIKA === undefined) { window.CHITIKA = { \'units\' : [] }; };'+
           'var unit = {"calltype":"async[2]","publisher":"'+Discourse.SiteSettings.chitika_publisher_code.trim()+'","width":'+width+',"height":'+height+',"sid":"Chitika Default"};'+
           'placement_id = window.CHITIKA.units.length;'+
@@ -22,6 +22,7 @@
           'divBox.setAttribute("id", block_id);'+
           'Pallet.appendChild(divBox);'+
         '}'+
+        'chitika_load();'+
         '</script>'+
         '<script type="text/javascript" src="//cdn.chitika.net/getads.js" async></script>');
       }
