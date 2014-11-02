5c9104a6968101cc5da7be86cf391dab4cded6b6
Ajmal Sali <ajmalmsali@gmail.com>
2014-11-02 14:40:16 +0530

    Update chitika.js

--- assets/javascripts/chitika.js
+++ assets/javascripts/chitika.js
@@ -9,14 +9,15 @@
     var mobile = ( slotid.search('_mobile') !== -1 ) ? true : false;
     if(!mobile){
       if (eval('Discourse.SiteSettings.chitika_show_' + slotid)) {
-        return new Handlebars.SafeString('<script type="text/javascript">'+
-          '(function() {'+
+        return new Handlebars.SafeString('<center><div id="'+slotid+'"></div></center>'+
+        '<script type="text/javascript">'+
+          'window.onload = function() {'+
           'if (window.CHITIKA === undefined) { window.CHITIKA = { \'units\' : [] }; };'+
           'var unit = {"calltype":"async[2]","publisher":"'+Discourse.SiteSettings.chitika_publisher_code.trim()+'","width":'+width+',"height":'+height+',"sid":"Chitika Default"};'+
-          'var placement_id = window.CHITIKA.units.length;'+
+          'placement_id = window.CHITIKA.units.length;'+
           'window.CHITIKA.units.push(unit);'+
-          'document.write(\'<center><div id="chitikaAdBlock-\'+placement_id+\'"></div></center>\');'+
-        '}());'+
+          'document.getElementById('+slotid+').innerHTML = '+'<div id="chitikaAdBlock-'+placement_id+'"></div>;'+
+        '}'+
         '</script>'+
         '<script type="text/javascript" src="//cdn.chitika.net/getads.js" async></script>');
       }
